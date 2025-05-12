b6be67be21a22bc9edea624930d59ab69cfb6ddb
Tore Green <tore@e-dreams.dk>
2025-05-13 00:36:10 +0200

    Add error message to bounce script

--- bounce/index.php
+++ bounce/index.php
@@ -37,7 +37,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['code'])) {
     $state = $_COOKIE['oauth_state'] ?? null;
     if (!$state) {
         http_response_code(400);
-        echo "Error: Missing state parameter";
+        echo "<b>Your configuration needs to be updated</b><br>";
+        echo "Thank you for using the iSolarCloud integration. <br>";
+        echo "Please reconfigure your Redirect URI in the ";
+        echo "<a href=\"https://developer-api.isolarcloud.com/\" target=\"_new\">Sungrow Developer Portal</a> to:<br>";
+        echo "<pre>$clientCallbackUri</pre>";
+        echo "and then try the authorisation flow again.<br><br>";
+        echo "(This message occurs after you have updated the iSolarCloud integration to v0.5.0 or newer which uses a new redirect URI.)";
         exit;
     }
 
